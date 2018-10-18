686758fd59e41ecc4cd55cfa2706b1fb222a9c41
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-18 15:20:16 +0200

    Fixed autowiring issue

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,10 @@
+package com.example;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableJpaRepositories({"com.example"})
 public class Main {
 
   //@Value("${spring.datasource.url}")
