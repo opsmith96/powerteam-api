d3f5932d3d9a4ce8d7c538bbde55c251be024967
Ole Petter Smith <opsmith96@gmail.com>
2018-10-19 14:36:40 +0200

    bet this wont work

---
+++ src/main/java/com/example/owner/Owner.java
@@ -0,0 +1,19 @@
+package com.example.owner;
+
+public class Owner {
+    int person_id;
+    int owner_id;
+
+    public Owner(int owner_id, int person_id) {
+        this.owner_id = owner_id;
+        this.person_id = person_id;
+    }
+
+    public int getPersonID() {
+        return person_id;
+    }
+
+    public int getContactID() {
+        return owner_id;
+    }
+}
