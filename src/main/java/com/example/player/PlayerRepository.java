d78d571821903745e0c648425c659617e91c5c53
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-19 08:09:28 +0200

    fghjk

--- src/main/java/com/example/player/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.player;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.awt.*;
-
-public interface PlayerRepository extends CrudRepository<Player,Integer> {
-    //public List<Player> findByTeamId(String teamid);
-}
