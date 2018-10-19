d3ecfd9c75b4068b4b92bc21e718672cbced298b
Ole Petter Smith <opsmith96@gmail.com>
2018-10-19 13:48:17 +0200

    CORS ?

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 @SpringBootApplication
 public class Main {
@@ -45,3 +46,4 @@ public class Main {
     }
   }
 }
+
