be0ae98911b57787210feda319c7b0ea8713c5bd
Ole Petter Smith <opsmith96@gmail.com>
2018-10-19 10:07:44 +0200

    get one person ?

--- src/main/java/com/example/person/PersonController.java
+++ src/main/java/com/example/person/PersonController.java
@@ -71,6 +71,38 @@ public class PersonController {
       return e.toString();
     }
   }
+
+  @GetMapping("/persons/{id}")
+  @ResponseBody
+  String getPerson(@PathVariable("id") int id){
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      String query = "SELECT * FROM person WHERE person_id = ?";
+      PreparedStatement preparedStatement = connection.prepareStatement(query);
+      preparedStatement.setInt(1, id);
+      preparedStatement.executeUpdate();
+      ResultSet rs = stmt.executeQuery(query);
+      ArrayList<Person> output = new ArrayList<>();
+      while (rs.next()) {
+
+        int person_id = rs.getInt("person_id");
+        String first_name = rs.getString("first_name");
+        String last_name = rs.getString("last_name");
+        Date date_of_birth = rs.getDate("date_of_birth");
+        int address_id = rs.getInt("address_id");
+
+        output.add(new Person(person_id, first_name, last_name, date_of_birth, address_id));
+
+      }
+
+      Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+      String prettyJson = prettyGson.toJson(output);
+
+      return prettyJson;
+    } catch (Exception e) {
+      return "error";
+    }
+  }
   
   @GetMapping("/persons")
   @ResponseBody
