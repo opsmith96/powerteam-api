f5047fa9d7f11394f6e6fbc7228609547ecadfd0
Ole Petter Smith <opsmith96@gmail.com>
2018-10-19 12:55:16 +0200

    try again

--- src/main/java/com/example/contact/ContactController.java
+++ src/main/java/com/example/contact/ContactController.java
@@ -37,20 +37,19 @@ public class ContactController {
   @ResponseBody
   String getContact(@PathVariable("id") int id) {
     try (Connection connection = dataSource.getConnection()) {
-      String query = "SELECT * FROM person WHERE person_id = ?";
+      String query = "SELECT * FROM contact WHERE contact_id = ?";
       PreparedStatement preparedStatement = connection.prepareStatement(query);
       preparedStatement.setInt(1, id);
       ResultSet rs = preparedStatement.executeQuery();
-      ArrayList<Person> output = new ArrayList<>();
+      ArrayList<Contact> output = new ArrayList<>();
       while (rs.next()) {
 
-        int person_id = rs.getInt("person_id");
-        String first_name = rs.getString("first_name");
-        String last_name = rs.getString("last_name");
-        Date date_of_birth = rs.getDate("date_of_birth");
-        int address_id = rs.getInt("address_id");
+        int contact_id = rs.getInt("contact_id");
+        String contact_type = rs.getString("contact_type");
+        String contact_detail = rs.getString("contact_detail");
 
-        output.add(new Person(person_id, first_name, last_name, date_of_birth, address_id));
+
+        output.add(new Contact(contact_id, contact_type, contact_detail));
 
       }
 
